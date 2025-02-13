f0d1b6844e2dbcd578b45aeefd2f363512339391
Praveenaa <praveenaa2408@gmail.com>
2025-02-13 14:34:57 -0700

    hello world

---
+++ portfolio-site/scripts/move-dist.js
@@ -0,0 +1,27 @@
+const fs = require('fs-extra');
+const path = require('path');
+
+// Get the paths
+const distPath = path.join(__dirname, '../dist');
+const rootPath = path.join(__dirname, '../../');
+
+async function moveFiles() {
+  try {
+    // Remove existing files in root (except .git and node_modules)
+    const existingFiles = await fs.readdir(rootPath);
+    for (const file of existingFiles) {
+      if (file !== '.git' && file !== 'node_modules' && file !== 'portfolio-site') {
+        await fs.remove(path.join(rootPath, file));
+      }
+    }
+
+    // Move all files from dist to root
+    await fs.copy(distPath, rootPath);
+    console.log('Successfully moved dist files to root');
+  } catch (err) {
+    console.error('Error moving dist files:', err);
+    process.exit(1);
+  }
+}
+
+moveFiles();
